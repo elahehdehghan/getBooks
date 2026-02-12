980f78e4507ee30b562eea89cba76f2ae1515f30
elahehdehghan <edbelaheh85@gmail.com>
2026-02-12 22:47:41 +0330

    add page parameter related functions

--- pl.py
+++ pl.py
@@ -1,21 +1,50 @@
 import requests
 import csv
+import os
 
+file_path = 'books.csv'
 url = 'http://openlibrary.org/search.json'
 
+def get_current_page():
+     if not os.path.exists('page.txt'):
+        return 1
+     with open('page.txt', "r") as f:
+        return int(f.read().strip())
+ 
+def save_page_num(page_num):
+    with open('page.txt', "w") as f:
+        f.write(str(page_num))
+    
+current_page = get_current_page()   
 par = {
     "q" : "law",
     "limit" : 50,
+    "page" : current_page,
     "fields" : "title,author_name,first_publish_year"
     }
 
 response = requests.get(url,params=par)
 data = response.jason()
+
+books = data.get('docs', [])
+num_found = data.get('num_found',0)
+total_pages = (num_found + par['limit'] - 1) // par['limit']
+
+if current_page > total_pages or len(books) == 0:
+    print("that was the last page.You will be returned to the first page.")
+    save_page_num(1)
+    exit()
+
 filtered_books = list(filter(lambda book: book.get('first_publish_year', 0) > 2000, data['docs']))
 
-output = open('books.csv', mode='w', newline='', encoding='utf-8')
-writer = csv.writer(output)
-writer.writerow(['Title', 'Author', 'Year'])
+if not os.path.exists(file_path) :
+    output = open(file_path, mode='w',newline='', encoding='utf-8') 
+    writer = csv.writer(output) 
+    writer.writerow(['Title', 'Author', 'Year']) 
+    
+else :
+    output = open(file_path, mode='a',newline='', encoding='utf-8')
+    writer = csv.writer(output)
 
 for book in filtered_books:
     title = book.get('title', 'Not Available')
@@ -25,3 +54,5 @@ for book in filtered_books:
 
 output.close() 
 print(f"{len(filtered_books)} books were saved to the file.")
+
+save_page_num(current_page + 1)
